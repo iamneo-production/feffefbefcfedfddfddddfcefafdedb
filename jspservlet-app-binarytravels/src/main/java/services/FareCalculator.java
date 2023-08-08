67e24d3db8e61ac50d4473ce871fc92ddfc2750e
Iamneo <techadmin@iamneo.ai>
2023-08-08 04:19:55 +0000

    Backup Commit

--- jspservlet-app-binarytravels/src/main/java/services/FareCalculator.java
+++ jspservlet-app-binarytravels/src/main/java/services/FareCalculator.java
@@ -8,5 +8,38 @@ import model.Hotel;
 import model.Train;
 
 public class FareCalculator extends Booking{
-//Enter code here...
+
+    public double book(Flight flight) {
+
+        double totalFare = booking(flight.getNoOfPersons(),flight.getRates());
+
+        if(flight.getTriptype().equalsIgnoreCase("one-way")){
+            return totalFare;
+        }else{
+            int comp = (flight.getFrom()).compareTo(flight.getTo());
+            if(comp < 0)
+                return 2 * totalFare;
+            else
+                return 0;
+        }
+    }
+
+    public double book(Bus bus) {
+        double totalFare = booking(bus.getNoOfPersons(),bus.getRates());
+        return totalFare;
+    }
+
+    public double book(Hotel hotel) {
+        double totalFare = 0;
+        if(hotel.getFromdate().isBefore(hotel.getTodate())){
+            totalFare = booking(hotel.getNoOfPersons(), hotel.getRates());
+        }
+        return totalFare;
+    }
+
+    public double book(Train train) {
+        double totalFare = booking(train.getNoOfPersons(),train.getRates());
+        return totalFare;
+    }
+
 }
